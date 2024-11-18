387be41ac2bbee59fe74f8d14b4c4ee0c0861902
Alejandro Villarreal <alejandro_3141230024@utd.edu.mx>
2024-11-17 23:23:54 -0600

    COMMIT P3 DJANGO V1

--- p3/practicasDjango3Bbis/proyectoUTDnormal/js/section.js
+++ p3/practicasDjango3Bbis/proyectoUTDnormal/js/section.js
@@ -1,9 +1,8 @@
-// Mostrar alerta al hacer clic en los enlaces del menú
+
 function showAlert(section) {
     alert(`Navegando a la sección: ${section}`);
 }
 
-// Cambiar el color de fondo del menú dinámicamente
 document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector("nav");
     nav.style.backgroundColor = "#333"; // Cambia el color de fondo
